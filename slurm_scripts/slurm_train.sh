fe59a4b2123d173c59f46cccf288fcf635a75119
Bohan Chen <wispcarey@gmail.com>
2025-06-27 13:58:13 -0700

    6/27

--- slurm_scripts/slurm_train.sh
+++ slurm_scripts/slurm_train.sh
@@ -2,12 +2,12 @@
 
 # Submit this script with: sbatch <this-filename>
 
-#SBATCH --time=2-00:00:00     # walltime (2 days)
+#SBATCH --time=1-00:00:00     # walltime (1 days)
 #SBATCH --nodes=1           # number of nodes (1 node)
-#SBATCH --gres=gpu:4        # 4 GPUs of any type
+#SBATCH --gres=gpu:1        # 4 GPUs of any type
 #SBATCH --partition=gpu     # use GPU partition
 #SBATCH --ntasks=1          # 1 task
-#SBATCH -J "bohan-gpu-LearnKalmanGain"   # job name
+#SBATCH -J "bohan-gpu-LearnLinear"   # job name
 #SBATCH --mail-user=bhchen@caltech.edu # email address
 #SBATCH --mail-type=BEGIN   # email notification at start
 #SBATCH --mail-type=END     # email notification at end
@@ -18,10 +18,10 @@
 #SBATCH -e slurm.%N.%j.err  # STDERR
 
 # Load modules if necessary (e.g., CUDA or other dependencies)
-# module load cuda/12.2  # Adjusted to CUDA version 12.2
+module load cuda/12.2  # Adjusted to CUDA version 12.2
 
 # Change to the directory containing v2_run_fine_tuning.sh
 cd ../scripts  # Assuming `scripts` folder is one level up from `slurm_script`
 
 # Run your program
-bash run_enscorrection_train.sh
+bash run_linear.sh
