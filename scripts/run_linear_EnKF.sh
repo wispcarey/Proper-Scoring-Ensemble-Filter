00d4034b756a30bbd0e1c3eb7fb3cc593972d212
Bohan Chen <bhchen@caltech.edu>
2025-06-29 20:32:37 -0700

    6/29 tests

--- scripts/run_linear_EnKF.sh
+++ scripts/run_linear_EnKF.sh
@@ -2,15 +2,14 @@
 
 cd ..
 
-# python train_v2.py \
-#     --epochs 500 \
-#     --dataset linear \
-#     --N 10 \
-#     --seed 42 \
-#     --v CorrTerms \
-#     --no_localization \
-#     --loss_type es \
-#     --es_p 1
+python train_v2.py \
+    --epochs 0 \
+    --dataset linear \
+    --N 10 \
+    --seed 42 \
+    --v EnKF \
+    --no_localization \
+    --normal_output
 
 # python train_v2.py \
 #     --epochs 500 \
@@ -43,15 +42,15 @@ cd ..
 #     --loss_type nes \
 #     --es_p 1 
 
-python train_v2.py \
-    --epochs 1500 \
-    --dataset linear \
-    --N 10 \
-    --seed 42 \
-    --v EtE2 \
-    --no_localization \
-    --loss_type es \
-    --es_p 1 
+# python train_v2.py \
+#     --epochs 1500 \
+#     --dataset linear \
+#     --N 10 \
+#     --seed 42 \
+#     --v EtE2 \
+#     --no_localization \
+#     --loss_type es \
+#     --es_p 1 
 
 # python train_v2.py \
 #     --epochs 500 \
