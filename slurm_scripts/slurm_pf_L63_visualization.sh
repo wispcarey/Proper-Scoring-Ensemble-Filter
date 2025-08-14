bc6fdbd86c2d1aaab47773aea7186f44e2b38a68
Bohan Chen <bhchen@caltech.edu>
2025-08-14 11:12:49 -0700

    new bpf

--- slurm_scripts/slurm_pf_L63_visualization.sh
+++ slurm_scripts/slurm_pf_L63_visualization.sh
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu     # use GPU partition
 #SBATCH --ntasks=1          # 1 task
-#SBATCH -J "bpf-rossl-visvis"   # job name
+#SBATCH -J "bpf-lorenz63-vis"   # job name
 #SBATCH --mail-user=bhchen@caltech.edu # email address
 #SBATCH --mail-type=BEGIN   # email notification at start
 #SBATCH --mail-type=END     # email notification at end
